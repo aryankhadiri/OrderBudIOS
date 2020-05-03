007f01e13000bfe708b50b55012084fe40a44804
Aryan Khadiri <aryankhadiri@gmail.com>
2020-05-03 00:07:44 -0700

    the search now searches the real data from the database

--- OrderBud/ViewController.swift
+++ OrderBud/ViewController.swift
@@ -9,6 +9,34 @@
 import UIKit
 import Parse
 class ViewController: UIViewController {
+    
+    
+    var restaurants = [PFObject]()
+    //var restaurantsNames:[String] = []
+    var restaurantsDict:[String:[String]]=[:]
+    
+    
+       func takingQuery(){
+           let query = PFQuery(className: "Restaurants")
+           query.includeKeys(["Name","city","state"])
+           query.findObjectsInBackground { (restaurants, error) in
+               if restaurants != nil{
+                   print("Restaurants Successfully Loaded")
+                   self.restaurants = restaurants!
+                print(self.restaurants.count)
+                for restaurant in self.restaurants {
+                    let name = restaurant["Name"] as! String
+                    let city = restaurant["city"] as! String
+                    let state = restaurant["state"] as! String
+                    self.restaurantsDict[name] = [city,state]
+                    //print(restaurant["Name"]!)
+                    //self.restaurantsNames.append(name)
+                }
+               }
+           }
+        
+       }
+    
    
     //This is fake Restaurant data 
     let RestaurantNameArr = [ "NoodleA", "NoodleB","NoodleC","Antarctica", "Antigua and Barbuda", "Argentina", "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia", "Bosnia and Herzegowina", "Botswana", "Bouvet Island", "Brazil", "British Indian Ocean Territory", "Brunei Darussalam", "Bulgaria", "Burkina Faso", "Burundi", "Cambodia", "Cameroon", "Canada", "Cape Verde", "Cayman Islands", "Central African Republic", "Chad"]
@@ -17,12 +45,12 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var tbView: UITableView!
     
-    
     @IBOutlet weak var SearchBar: UISearchBar!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        takingQuery()
+            // Do any additional setup after loading the view.
     }
 }
 extension ViewController: UITableViewDelegate, UITableViewDataSource {
@@ -39,11 +67,13 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") as! searchTableViewCell
         if searching {
             cell.nameLabel.text = searchedRestaurant[indexPath.row]
-            cell.cityLabel.text = "San Jose"
-            cell.stateLabel.text = "CA"
+            //cell.nameLabel.text = restaurants[indexPath.row]["Name"] as! String
+            
+            cell.cityLabel.text = restaurantsDict[searchedRestaurant[indexPath.row]]?[0]
+            cell.stateLabel.text = restaurantsDict[searchedRestaurant[indexPath.row]]?[1]
             //cell?.textLabel?.text = searchedRestaurant[indexPath.row]
         } else {
-            cell.nameLabel.text = RestaurantNameArr[indexPath.row]
+            //cell.nameLabel.text = RestaurantNameArr[indexPath.row]
             cell.cityLabel.text = "San Jose"
             cell.stateLabel.text = "CA"
             //cell?.textLabel?.text = RestaurantNameArr[indexPath.row]
@@ -53,12 +83,22 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
 }
         
 extension ViewController: UISearchBarDelegate {
-        
+
         func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-           searchedRestaurant = RestaurantNameArr.filter({$0.lowercased().prefix(searchText.count) == searchText.lowercased()})
+            
+            searchedRestaurant = restaurantsDict.keys.filter({$0.lowercased().prefix(searchText.count) == searchText.lowercased()})
+
+           //searchedRestaurant = RestaurantNameArr.filter({$0.lowercased().prefix(searchText.count) == searchText.lowercased()})
+            if searchText == ""{
+                searching = false
+                tbView.reloadData()
+            }
+            else{
             searching = true
             tbView.reloadData()
-        }
+            }
+            }
+            
         
         func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
             searching = false
